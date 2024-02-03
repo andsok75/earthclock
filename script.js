61d9c54e05a1af2e37b05b2afae9fd1f3a683796
Andrey Sokolov <andsok@fastmail.com>
2024-02-03 05:13:47 +0000

    add curl command to get apparent to mean noon difference

--- script.js
+++ script.js
@@ -40,17 +40,30 @@ function getSecondsToLocalNoon(secondOfDay, secondOfDayAtLocalNoon) {
 }
 
 function printTime() {
+    // This function prints the standard time, that is UTC time expressed in decimal units, and local time.
+    // The time at local noon is the standard time when the mean sun passes the local medirian.
+    // The local time is the time to local noon expressed in decimal units.
+    // The pm period tells you how much time has passed since noon, and the am period tells you how much time remains until noon.
+    // For instance, 1:95am mean there are 195 decimal minutes remaining until noon at the given location.
+    // Note that the mean sun is usually different from the apparent sun, so apparent noon may be ahead or behind the mean noon by a few minutes.
+    // For details, see https://en.wikipedia.org/wiki/Equation_of_time
+    //
+    // The following command yields a table showing decimal minutes from mean noon to apparent noon for each day of year 2024.
+    // For instance, on day 40 the apparent noon occurs later than the mean noon by almost ten decimal minutes.
+    // For details, see https://ssd-api.jpl.nasa.gov/doc/horizons.html
+    // (YEAR=2024; curl -s "https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='10'&OBJ_DATA='NO'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='0,0,0@399'&START_TIME='"$YEAR"-01-01%2012:00'&STOP_TIME='"$YEAR"-12-31%2012:00'&STEP_SIZE='1%20d'&CSV_FORMAT='NO'&QUANTITIES='34'" | awk 'NR==1{printf "day minutes\n"}/\$\$SOE/{flag=1;next}/\$\$EOE/{flag=0}flag{h=$(NF-2);m=$(NF-1);s=$NF;eot=-(((h-12)*60+m)*60+s)*10/864; printf "%3d%7.2f\n",i++,eot}' -)
+
     const date = new Date();
     const dayOfYear = getDayOfYear(date);
     const secondOfDay = getDecimalSecondOfDay(date);
 
-    const global = {
+    const standard = {
         year: getYear(date),
         week: Math.trunc(dayOfYear/10),
         ...getTimeFromDecimalSeconds(secondOfDay, dayOfYear%10)
     };
 
-    console.log("global:",  global);
+    console.log("standard:",  standard);
 
     if (!navigator.geolocation) { return; }
 
