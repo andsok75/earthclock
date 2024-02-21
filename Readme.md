44cf2bc622692866ffdeace3645cf07190a3a266
Andrey Sokolov <andsok@fastmail.com>
2024-02-21 03:53:21 +0000

    add compression script

--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
-Countries GeoJSON from https://datahub.io/core/geo-countries with the precision reduced to three digits after the dot
+Countries GeoJSON from https://datahub.io/core/geo-countries
+
+Compress file by converting to lower precision, e.g. 2, and removing spaces with jq
+```
+python compress_geojson.py countries.geojson 2
+jq -rc . countries.geojson | sponge countries.geojson
+```
---
+++ compress_geojson.py
@@ -0,0 +1,42 @@
+import json
+import sys
+
+def print_fields(d, path=''):
+    if path:
+        print(path)
+    if isinstance(d, list) and len(d) != 0:
+        print_fields(d[0], path + "[]")
+    if not isinstance(d, dict):
+        return
+    for k in d.keys():
+        print_fields(d[k], path + f"/{k}")
+
+def round_point(point, n):
+    return [round(point[0], n), round(point[1], n)]
+
+
+file_name = sys.argv[1]
+precision = int(sys.argv[2])
+
+print(f"converting file {file_name} to precision {precision}")
+
+countries = json.load(open(file_name))
+
+print_fields(countries)
+print(set(f["geometry"]["type"] for f in countries["features"]))
+
+for f in countries["features"]:
+    t = f["geometry"]["type"]
+    if t == "Polygon":
+        polygon = f["geometry"]["coordinates"][0]
+        for i, point in enumerate(polygon):
+            polygon[i] = round_point(point, precision)
+    elif t == "MultiPolygon":
+        for polygons in f["geometry"]["coordinates"]:
+            for polygon in polygons:
+                for i, point in enumerate(polygon):
+                    polygon[i] = round_point(point, precision)
+    else:
+        raise Exception(f"unexpected polygon type {t}")
+
+json.dump(countries, open(file_name, "w"))
